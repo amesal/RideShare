9b9927ba6b91120189e0e2ccf09ecdd8d10bb6d1
amesal <asalette26@students.polytechnic.org>
2025-02-24 09:11:38 -0800

    added

---
+++ Passenger.java
@@ -0,0 +1,38 @@
+package RideShare;
+import java.util.*;
+public class Passenger(){
+    private int destination;
+    private boolean direction; // true for goign right, false for left
+    private int startLocation; //could be redundant to ask for every location along the way but starting location could be helpful
+
+    //constructors
+    public Passenger(int myDestination, int myStart){
+        destination = myDestination;
+        startLocation = myStart;
+        if(destination > startLocation){
+            direction = false; //bc wanting to go left
+        } else {
+            direction = true;
+        }
+
+        //could replace if/else with "direction = destination > startLocation"
+
+    }
+
+    public int getDestination(){
+        return destination;
+    }
+
+    public boolean getDirection(){
+        return direction;
+    }
+
+    public int getStart(){
+        return startLocation;
+    }
+
+    public String toString(){
+        return super.toString() + " Start: " + startLocation + " Destination: " + destination + " Going right? " + direction; //boolean based on going right or not
+    }
+
+}
---
+++ Tester.java
@@ -0,0 +1,12 @@
+package RideShare;
+public class Tester {
+    public static void main(String[] args){
+        Passenger a = new Passenger(5, 7);
+        Passenger b = new Passenger(2, 0);
+        Passenger c = new Passenger(10, 24);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+    
+}
