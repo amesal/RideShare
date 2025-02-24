db59d915fd7e6725bca65abcc46b6ee6bc78f439
amesal <asalette26@students.polytechnic.org>
2025-02-24 09:29:42 -0800

    added?

---
+++ Car.java
@@ -0,0 +1,29 @@
+package RideShare;
+import java.util.*;
+public class Car {
+    //fields
+    private int destination;
+    private int currentLocation;
+    private boolean direction;
+    private ArrayList<Passenger> customers;
+
+    public Car(int myDestination, int start){
+        destination = myDestination;
+        currentLocation = start;
+        direction = destination > currentLocation; //boolean comparison
+        customers = new ArrayList<Passenger>();
+    }
+
+    public String toString(){
+        return super.toString() + "Destination: " + destination + " Current location: " + currentLocation + " Going right? " + direction + " Passengers: " + customers;
+    }
+
+    public void addPassenger(Passenger p){
+        if(customers.size() >= 3){
+            System.out.println("error: no room!");
+        } else {
+            customers.add(p);
+        }
+    }
+    
+}
--- Tester.java
+++ Tester.java
@@ -7,6 +7,10 @@ public class Tester {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+        Car n = new Car(5, 9);
+        System.out.println(n);
+        n.addPassenger(a);
+        System.out.println(n);
     }
     
 }
