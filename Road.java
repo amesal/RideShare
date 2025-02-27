890a35a185970e8fdcd1b49b439e322d8e6ea602
amesal <asalette26@students.polytechnic.org>
2025-02-27 14:49:56 -0800

    added displaySummary() for ending statement

--- Road.java
+++ Road.java
@@ -1,4 +1,37 @@
+/**
+ * Essentially the manager of the program.
+ */
+
 package RideShare;
+import java.util.*;
 public class Road {
     
+    private ArrayList<Station> stationTracker = new ArrayList<Station>();
+    private ArrayList<Car> carTracker = new ArrayList<Car>();
+
+    public Road(int roadSize){
+        stationTracker = new ArrayList<Station>();
+        carTracker = new ArrayList<Car>();
+
+    }
+
+    public String displayStationStatus(){
+        String stationLog = "";
+        for(Station a : stationTracker){
+            stationLog += a.toString() + "\n";
+        }
+        return stationLog;
+    }
+
+    public String displaySummary(){
+        String tripSummary = "";
+        int totalMilesTraveled = 0;
+        for(Car a : carTracker){
+            totalMilesTraveled += a.milesTraveled();
+        }
+        return (tripSummary += totalMilesTraveled);
+    }
+
 }
+
+
